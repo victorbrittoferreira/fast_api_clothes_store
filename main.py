f4f78f9fc6c82673a2803f233c4e0a0f4ba2a76c
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2022-12-20 15:13:38 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import enum
+
+import databases
+import sqlalchemy
+from decouple import config
+from fastapi import FastAPI
+
+DATABASE_URL = (
+    f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
+    + f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
+)
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+# M O D E L
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String(120), unique=True),
+    sqlalchemy.Column("password", sqlalchemy.String(255)),
+    sqlalchemy.Column("full_name", sqlalchemy.String(200)),
+    sqlalchemy.Column("phone", sqlalchemy.String(13)),
+    sqlalchemy.Column(
+        "created_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+    ),
+    sqlalchemy.Column(
+        "last_modified_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now(),
+    ),
+)
+
+
+class ColorEnum(enum.Enum):
+    pink = "pink"
+    black = "black"
+    white = "white"
+    yellow = "yellow"
+
+
+class SizeEnum(enum.Enum):
+    xs = "xs"
+    s = "s"
+    m = "m"
+    l = "l"
+    xl = "xl"
+    xxl = "xxl"
+
+
+clothes = sqlalchemy.Table(
+    "clothes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(120)),
+    sqlalchemy.Column("color", sqlalchemy.Enum(ColorEnum), nullable=False),
+    sqlalchemy.Column("size", sqlalchemy.Enum(SizeEnum), nullable=False),
+    sqlalchemy.Column("photo_url", sqlalchemy.String(255)),
+    sqlalchemy.Column(
+        "created_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+    ),
+    sqlalchemy.Column(
+        "last_modified_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now(),
+    ),
+)
+
+app = FastAPI()
+
+# M I D D L E W A R E
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
